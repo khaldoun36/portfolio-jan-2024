45afec14d1d343cbecf3defe698ed7de4bef9402
khaldoun36 <khaldoun36@gmail.com>
2024-01-25 15:08:37 +0400

    fixed font loading bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
       link: [
         {
           rel: "preload",
-          href: "/_nuxt/fonts/GeistVariableVF.woff2",
+          href: "/fonts/GeistVariableVF.woff2",
           crossorigin: "",
           as: "font",
           type: "font/woff2",
